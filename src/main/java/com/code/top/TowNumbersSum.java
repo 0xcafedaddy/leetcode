6074420b626b2edcb74e1230f556e83d71c9d429
kunyang <importy@163.com>
2019-06-24 22:38:56 +0800

    两数之和

--- src/main/java/com/code/top/TowNumbersSum.java
+++ src/main/java/com/code/top/TowNumbersSum.java
@@ -21,5 +21,50 @@ package com.code.top;
 public class TowNumbersSum {
 
 
+    public static void main(String[] args) {
+
+    }
+
+
+    /**
+     * 解法1:
+     *  最简单直接的解法。时间复杂度最高
+     *  第一次循环一次数组，获取当前的数组下标的的值。然后再依次获取到此数组下标之后的所有的所有元素，和此元素相加，判断是否等于目标值，相等即退出。
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum1(int[] nums, int target) {
+        return null;
+    }
+
+
+    /**
+     * 解法2：
+     *  使用两次循环，第一次按照 [元素-下标] 的形式存入map。
+     *  第二次循环一次数组，用target - 循环元素 = 差值。
+     *  再通过 map.getkey 的方式去查找 是否存在这个差值，如果存在 即退出循环。
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        return null;
+    }
+
+
+    /***
+     * 解法3：
+     *  使用一次循环，在循环数组的同时将元素添加进map集合。
+     *  在每次添加元素之前，先进行 差值 判断，是否存在在map中
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum3(int[] nums, int target) {
+        return null;
+    }
+
+
 
 }
